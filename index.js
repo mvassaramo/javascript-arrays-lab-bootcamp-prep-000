88fa1a48510dcce54204567b6d1bdba2692e7e93
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-04-28 16:11:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,7 @@ function destructivelyRemoveLastKitten() {
   return kittens;
 }
 
-function destructivelyRemoveFirstKitten()
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
