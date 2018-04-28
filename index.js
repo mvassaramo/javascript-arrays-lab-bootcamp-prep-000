69b93b6dc0fc29be464d9194e6e5428f890af61d
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-04-28 15:12:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 var kitten = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(kitten,newKitten) {
-  return kitten.push(newKitten);
+  return kitten.push (newKitten);
 }
 
